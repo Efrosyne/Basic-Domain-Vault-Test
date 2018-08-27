eb2b79f0881377c6ab01d269b6e38f965ce754f5
Efrosyne <39443819+Efrosyne@users.noreply.github.com>
2018-08-27 16:28:08 +0200

    Add files via upload

--- api_test.py
+++ api_test.py
@@ -15,7 +15,7 @@ def dict_factory(cursor, row):
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>KOSMOS</h1>
-<p>A prototype API for test API CRUD functionality.</p>'''
+<p>A prototype API for try API functionality.</p>'''
 
 
 @app.route('/api/v1/resources/kosmos/all', methods=['GET'])
@@ -23,9 +23,9 @@ def api_all():
     conn = sqlite3.connect('kosmos.db')
     conn.row_factory = dict_factory
     cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM kosmos;').fetchall()
+    all_people = cur.execute('SELECT * FROM kosmos;').fetchall()
 
-    return jsonify(all_books)
+    return jsonify(all_people)
 
 
 
@@ -33,14 +33,40 @@ def api_all():
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
+@app.route('/', methods=['GET','POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+	return jsonify({'you sent': some_json}),201
+    else:
+	return jsonify({'about' : 'Hello World!'}),201
+	 
 
-@app.route('/api/v1/resources/kosmos', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
+def delete():
+    conn = sqlite3.connect('kosmos.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cursor.execute('''DELETE FROM kosmos WHERE ip = ?''', (delete_ip,))
+    db.commit()
+
+@app.route('/', methods=['GET','POST'])
+def update():
+    conn = sqlite3.connect('kosmos.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cursor.execute('''UPDATE kosmos SET name = ? WHERE id = ?''', (newName, kosmos_id))
+
+
+
+
+@app.route('/api/v1/resources/kosmos', methods=['GET','POST'])
 def api_filter():
     query_parameters = request.args
 
     id = query_parameters.get('id')
-    published = query_parameters.get('name')
-    author = query_parameters.get('ip')
+    name = query_parameters.get('name')
+    ip = query_parameters.get('ip')
 
     query = "SELECT * FROM kosmos WHERE"
     to_filter = []
@@ -48,13 +74,13 @@ def api_filter():
     if id:
         query += ' id=? AND'
         to_filter.append(id)
-    if published:
+    if name:
         query += ' name=? AND'
-        to_filter.append(published)
-    if author:
+        to_filter.append(name)
+    if ip:
         query += ' ip=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
+        to_filter.append(ip)
+    if not (id or name or ip):
         return page_not_found(404)
 
     query = query[:-4] + ';'
@@ -66,6 +92,8 @@ def api_filter():
     results = cur.execute(query, to_filter).fetchall()
 
     return jsonify(results)
+
+
 
 app.run()
 
